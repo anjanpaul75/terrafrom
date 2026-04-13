06e5960b30c5d386c2d66019abb6fd5d2ab1e2aa
anjanpaul75 <115606379+anjanpaul75@users.noreply.github.com>
2026-04-13 22:14:37 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,20 @@
-Sample Terraform Code:
+module "aks_arc" { 
+# Make sure to use the latest AVM module version
+source = "Azure/avm-res-hybridcontainerservice-provisionedclusterinstance/azurerm" 
+version = "~>2.0"
 
-provider "azurerm" {
-features {}
-}
+# Make sure to provide all required parameters; for example, location= = "eastus"
+resource_group_id = "/subscriptions/your id/resourceGroups/az-local-workload-rg" 
+location = "southeastasia" 
+name = "bbl-aks-cluster24460" 
+logical_network_id = "/subscriptions/your id/resourceGroups/az-local-workload-rg/providers/microsoft.azurestackhci/logicalnetworks/BBL-AZPOC-SERVICE" 
+custom_location_id = "/subscriptions/your id/resourceGroups/rgazlsabbl/providers/Microsoft.ExtendedLocation/customLocations/bblazdc" 
+agent_pool_profiles = [{count=1}] 
+ssh_public_key =  "your key"
 
-resource "azurerm_resource_group" "rg" {
-name = "aks-rg"
-location = "East US"
-}
-
-resource "azurerm_kubernetes_cluster" "aks" {
-name = "my-aks-cluster"
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
-dns_prefix = "myaks"
-
-default_node_pool {
-name = "nodepool1"
-node_count = 1
-vm_size = "Standard_DS2_v2"
-}
-
-identity {
-type = "SystemAssigned"
-}
+# Optional parameters, update them as needed
+enable_azure_rbac = false
+enable_workload_identity = false 
+enable_oidc_issuer = false 
+rbac_admin_group_object_ids = ["group id"]
 }
